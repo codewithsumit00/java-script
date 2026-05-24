56ff5e22530f63748cb454079550a7ae1f8b8c6c
sumit gautam <gautamsumit737@gmail.com>
2026-05-24 23:30:45 +0530

    update file

---
+++ prectice5.js
@@ -0,0 +1,9 @@
+// let fullname  = prompt ("enter your fullname without space ");
+// let  username  = "@" + fullname + fullname.length;
+// console.log (username);
+
+let fullName = prompt("enter your fullname without spaces");
+
+let username = "@" + fullName + fullName.length;
+
+console.log(username);
--- program4Game.js
+++ program4Game.js
@@ -7,12 +7,28 @@
 
 //} 
 
-let gamenumber = 50;
-let usernumber = prompt ("guess the number between 1 to 100 ");
-if(usernumber != gamenumber){
-    usernumber = prompt ("please try again guess the number between 1 to 100 ");
+// let gamenumber = 50;
+// let usernumber = prompt ("guess the number between 1 to 100 ");
+// if(usernumber != gamenumber){
+//     usernumber = prompt ("please try again guess the number between 1 to 100 ");
 
-} else {
-    console.log ("congratulation you win the match ");
-}
-   
+// } else {
+//     console.log ("congratulation you win the match ");
+// }
+
+
+//string Method in js 
+
+// let str = "hello js ";
+// let str1 ="HELLLOMR JS";
+// console. log (str.length);
+// console.log (str.toUpperCase);
+// console.log (str1.toLowerCase);
+// console.log (str1.slice(0,5));
+// console.log (str1.slice(6,8));
+// console.log (str1.replace("MR","MR."));
+
+
+//
+let str = "12365866";
+console.log(str.slice(7,1));9
