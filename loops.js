e51de1c674f0dfd29b3e598ed1b42aa7e1a9797d
sumit gautam <gautamsumit737@gmail.com>
2026-05-24 11:37:01 +0530

    update profile

---
+++ loops.js
@@ -0,0 +1,49 @@
+// for(let i = 1; i<=5; i++){
+//     console.log("hello dear");
+// }
+
+// //calculate the sum of 1 to 10 num 
+// let sum = 0;
+// n=50;
+// for (let i=0; i <= n; i++){
+//     sum = sum + i;
+// }
+// console.log ( "sum", sum  );
+
+
+// // while loop 
+// let i = 10;
+// while (i<=10 ){
+//     console.log ("hello miss",i);
+//     i++
+
+//}
+//do while loop 
+// let i = 1;
+// do {
+//     console.log("hello miss", i);
+//      i++;
+// }while (i<=10);
+
+//for 0f loop 
+// let str = "hellodeveloper";
+
+// let size = 0;
+// for (let i of str){
+//     console.log (i);
+//     size++;
+
+// } console.log ("size", size );
+
+//for-in loop 
+
+let student={
+    name: "martin",
+    age: 22,
+    city: "delhi",
+    cgpa: 8.5,
+    ispass: true,
+}
+for (let key in student ){
+    console.log ("key = ",key, "value  =",student [key ]);
+}
---
+++ program4Game.js
@@ -0,0 +1,18 @@
+// let gamenumber =50;
+// let usernumber = prompt ("guess the number between 1 to 100 ");
+// if(usernumber ==gamenumber){
+//     console.log ("congratulation you win the match ");
+// } else{
+//   console.log("sorry lose the match try again ");
+
+//} 
+
+let gamenumber = 50;
+let usernumber = prompt ("guess the number between 1 to 100 ");
+if(usernumber != gamenumber){
+    usernumber = prompt ("please try again guess the number between 1 to 100 ");
+
+} else {
+    console.log ("congratulation you win the match ");
+}
+   
