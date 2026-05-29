0f42b3c11756690c86559bc64204583d46634fd1
sumit gautam <gautamsumit737@gmail.com>
2026-05-29 23:52:05 +0530

    update my information

---
+++ function.js
@@ -0,0 +1,27 @@
+// function myfirstfunction (){
+//     console.log ("this is a function");
+//     console.log ("we are learn function:");
+
+// }
+// myfirstfunction();
+// myfirstfunction();
+// myfirstfunction();
+// myfirstfunction();
+// myfirstfunction();
+
+
+// function myfunction(msg){
+//     console.log(msg); //parameter use for take a input  
+// }
+
+// myfunction(" i love js ") // argument 
+// myfunction(" this language very usefull for web development ") // argument 
+
+//calculate the some of two numbers by a function 
+function someofnumbers(x,y){
+   //console.log(x+y);
+   s = x+y;
+   return s;
+}
+let val = someofnumbers (45,5);
+console.log (val);
