e2ce6369945a6b6bb6b9b4221c7054288962ba99
sumit gautam <gautamsumit737@gmail.com>
2026-05-29 22:16:30 +0530

    update profile

--- arrayPrectice7.js
+++ arrayPrectice7.js
@@ -8,3 +8,23 @@ for (let val of item ){
 
     idx++;
 }
+// array methods 
+let item1 =[250,645 ,300,900,50];
+let offer1 = item1.map (val => val - (val /10));
+console.log (offer1);
+// slice method
+let item2 =[250,645 ,300,900,50];
+let offer2 = item2.slice (1,2);
+console.log (offer2);
+
+//array slice and ssplice methods 
+
+// let array =[1,2,3,4,5,6,7,];
+//  array.splice (2,4,101,101,103,104);
+//   console.log (array ); 
+
+let array =[1,2,3,4,5,6,7,]; //add value at index 
+    array.splice (1,0,102);
+    console.log (array);
+
+    
---
+++ arrayPrectice8.js
@@ -0,0 +1,17 @@
+ let companies = ["bloomberg", "microsoft","uber" ,"google", "apple", "amazon"];
+// companies.splice (0,1);
+// console.log (companies);  // task one remove bloomberg from the array
+
+//task 2 remove uber and add ola in this place
+// companies.splice (2,1,"ola");
+// console.log (companies);
+
+
+// add amazon at the end of the array
+// count= companies.length;
+// console.log (count);
+// companies.splice (companies,0, "netflix ");
+// console.log (companies);
+
+companies.push("netflix");
+console.log (companies);
