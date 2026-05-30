aa0280f745c144db859ab9668eb220515e352186
sumit gautam <gautamsumit737@gmail.com>
2026-05-30 21:19:09 +0530

    update information

--- function.js
+++ function.js
@@ -18,10 +18,34 @@
 // myfunction(" this language very usefull for web development ") // argument 
 
 //calculate the some of two numbers by a function 
-function someofnumbers(x,y){
-   //console.log(x+y);
-   s = x+y;
-   return s;
+// function someofnumbers(x,y){
+//    //console.log(x+y);
+//    s = x+y;
+//    return s;
+// }
+// let val = someofnumbers (45,5);
+// console.log (val);
+
+// arrow function 
+// const arrowsum = (a,b )=>{
+//   console.log(a+b);
+//  }
+
+// //arrowsum(56,4);
+
+// //function mul(a,b){
+//   // return a*b;
+
+
+// const arrowmul = (a,b)=>{
+//    console.log (a*b);
+// }
+// arrowmul(6,9);
+
+function div(a,b){
+console.log(a/b);
 }
-let val = someofnumbers (45,5);
-console.log (val);
+ let arrowdiv=(a,b)=>{
+   return a/b;
+ };
+div(5,2);
