1589bcced76beb5b97fc8c761e2f7d4ebea3f4f6
sumit gautam <gautamsumit737@gmail.com>
2026-06-02 19:45:50 +0530

    update profile

--- DOM.js
+++ DOM.js
@@ -1,2 +1,10 @@
-console.log("hello DOM");
-alert("first check this page ");
+// console.log("hello DOM");
+// alert("first check this page ");
+
+// // querySelector
+// let element = document.querySelector("h1");
+// console.log (element);
+
+//DOM manipulatio  
+div.innerHTML = "<h1>hello DOM</h1>";
+console.dir(documetn.body.firstChild);
