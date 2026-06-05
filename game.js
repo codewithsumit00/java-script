bf3b1c54feb5fc9cea6b250f68c11b693f8ec8d5
sumit gautam <gautamsumit737@gmail.com>
2026-06-05 22:16:46 +0530

    update profile

---
+++ game.js
@@ -0,0 +1,53 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn =document.querySelector("#reset");
+let newGamebtn =document.querySelector("#new-btn");
+let msgcontainer =document.querySelector("#msg-container");
+let msg  =  document.querySelector("#msg");
+
+
+let turn0 = true;
+
+const winPatterns = [
+   [0, 1, 2],
+   [ 0, 3, 6],
+   [0, 4, 8],
+   [1,4, 7],
+   [ 2,5,8],
+   [2,4,6],
+   [3,4 ,5],
+   [6,7,8],
+];
+
+
+boxes.forEach((box ) =>{
+ box.addEventListener("click" , () =>{
+    console.log("box was clicked");
+
+    if(turn0){
+        box.innerText ="0";
+        turn0 = false;
+    } else{
+        box.innerText ="X";
+        turn0 = true;
+    }
+    box.disabled = true;
+    
+    checkWinner(); 
+ });
+});
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+
+            //console.log(pattern[0], pattern[1], pattern[2]);
+        let pos1 =boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+
+        if (pos1 != "" &&pos2 != ""&& pos3 !=""){
+            if (pos1 === pos2 && pos1===pos3){
+                console.log("winners" , pos1);
+            }
+        }
+    }
+};
