36f3b5306544c978e49a8c86ac23ade7c210084f
sumit gautam <gautamsumit737@gmail.com>
2026-06-06 20:01:27 +0530

    update profile

---
+++ TODO_TicTacToe_fix.md
@@ -0,0 +1,5 @@
+- [ ] Fix game.html new game button id typo
+- [ ] Fix game.js: color of X and color of O different; winning message exactly "Congratulations! Game is guide"; correct symbols X and O
+- [ ] Fix game.css: add styles for .box-x and .box-o; fix #newbtn selector if needed
+- [ ] Test in browser (open game.html) and verify winner + reset/new game work
+
--- game.js
+++ game.js
@@ -1,53 +1,89 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn =document.querySelector("#reset");
-let newGamebtn =document.querySelector("#new-btn");
-let msgcontainer =document.querySelector("#msg-container");
-let msg  =  document.querySelector("#msg");
+let resetBtn = document.querySelector("#reset");
+let newGamebtn = document.querySelector("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-
-let turn0 = true;
+let turn0 = true; // true -> X, false -> O
 
 const winPatterns = [
-   [0, 1, 2],
-   [ 0, 3, 6],
-   [0, 4, 8],
-   [1,4, 7],
-   [ 2,5,8],
-   [2,4,6],
-   [3,4 ,5],
-   [6,7,8],
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
-
-boxes.forEach((box ) =>{
- box.addEventListener("click" , () =>{
-    console.log("box was clicked");
-
-    if(turn0){
-        box.innerText ="0";
-        turn0 = false;
-    } else{
-        box.innerText ="X";
-        turn0 = true;
-    }
+const disableBoxes = () => {
+  for (let box of boxes) {
     box.disabled = true;
-    
-    checkWinner(); 
- });
-});
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+  }
+};
+
+const showWinner = (winner) => {
+  // Show only when a winning pattern is found
+  msg.innerText = `Congratulations! ${winner} wins`;
+  msgcontainer.classList.remove("hide");
+  disableBoxes();
+};
+
 
 const checkWinner = () => {
-    for(let pattern of winPatterns){
+  for (let pattern of winPatterns) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
 
-            //console.log(pattern[0], pattern[1], pattern[2]);
-        let pos1 =boxes[pattern[0]].innerText;
-        let pos2 = boxes[pattern[1]].innerText;
-        let pos3 = boxes[pattern[2]].innerText;
-
-        if (pos1 != "" &&pos2 != ""&& pos3 !=""){
-            if (pos1 === pos2 && pos1===pos3){
-                console.log("winners" , pos1);
-            }
-        }
+    if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+      if (pos1 === pos2 && pos1 === pos3) {
+        showWinner(pos1);
+        return;
+      }
     }
+  }
 };
+
+const resetGame = () => {
+  turn0 = true;
+  enableBoxes();
+  for (let box of boxes) {
+    box.innerText = "";
+    box.classList.remove("box-x");
+    box.classList.remove("box-o");
+  }
+  msgcontainer.classList.add("hide");
+};
+
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turn0) {
+      box.innerText = "X";
+      box.classList.add("box-x");
+      box.classList.remove("box-o");
+      turn0 = false;
+    } else {
+      box.innerText = "O";
+      box.classList.add("box-o");
+      box.classList.remove("box-x");
+      turn0 = true;
+    }
+
+
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+
+resetBtn.addEventListener("click", resetGame);
+if (newGamebtn) newGamebtn.addEventListener("click", resetGame);
